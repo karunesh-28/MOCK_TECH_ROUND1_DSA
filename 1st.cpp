85824a1517de95cfe522bdb7cac417e1bff34cbd
karunesh vijay chikne <142775824+karunesh-28@users.noreply.github.com>
2025-01-17 14:59:52 +0530

    1st.cpp

--- 1st.cpp
+++ 1st.cpp
@@ -9,72 +9,68 @@ struct Node {
     Node* next;  // Pointer to the next Node
 };
 
-// Function to reverse a linked list
 Node* reverseList(Node* head) {
     Node *prev = NULL, *current = head, *next = NULL;
 
-    // Traverse the list and reverse the links between nodes
+    
     while (current) {
-        next = current->next;   // Save the next node
-        current->next = prev;   // Reverse the current node's link
-        prev = current;         // Move the prev pointer forward
-        current = next;         // Move the current pointer forward
+        next = current->next;   
+        current->next = prev;   
+        prev = current;         
+        current = next;        
     }
-    return prev;  // The prev pointer will now be the new head
-}
+    return prev;  
 
-// Function to detect if a linked list contains a loop
 bool detectLoop(Node* head) {
     Node *slow = head, *fast = head;
 
-    // Use two pointers: slow moves one step, fast moves two steps
+    
     while (fast && fast->next) {
-        slow = slow->next;          // Move slow pointer one step
-        fast = fast->next->next;    // Move fast pointer two steps
+        slow = slow->next;       
+        fast = fast->next->next;  
 
-        if (slow == fast) {         // If they meet, there's a loop
+        if (slow == fast) {       
             return true;
         }
     }
-    return false;  // No loop found
+    return false;
 }
 
-// Function to remove a loop in the linked list
+
 void removeLoop(Node* head) {
     Node *slow = head, *fast = head;
 
-    // Detect the loop using two pointers
+   
     while (fast && fast->next) {
         slow = slow->next;
         fast = fast->next->next;
 
-        if (slow == fast) {  // Loop detected
-            slow = head;  // Move one pointer to the head
+        if (slow == fast) {  
+            slow = head;  
 
-            // Find the starting point of the loop
+           
             while (slow->next != fast->next) {
                 slow = slow->next;
                 fast = fast->next;
             }
 
-            fast->next = NULL;  // Break the loop by setting the link to NULL
+            fast->next = NULL;
             return;
         }
     }
 }
 
-// Function to print the elements of a linked list
+
 void printList(Node* head) {
-    while (head) {  // Traverse the list until the end (NULL)
-        cout << head->data << " ";  // Print each node's data
-        head = head->next;          // Move to the next node
+    while (head) {  
+        cout << head->data << " ";
+        head = head->next;        
     }
-    cout << endl;  // End the line after printing the list
+    cout << endl; 
 }
 
-// Main function to demonstrate the linked list operations
 int main() {
-    // Create nodes manually
+
     Node* head = new Node();
     head->data = 1;
     head->next = new Node();
@@ -85,20 +81,19 @@ int main() {
     head->next->next->next->data = 4;
     head->next->next->next->next = NULL;
 
-    // Create a loop (4 -> 2)
+
     head->next->next->next->next = head->next;
 
-    // Detect and remove loop if present
+
     if (detectLoop(head)) {
         cout << "Loop detected. Removing loop...\n";
         removeLoop(head);
     }
 
-    // Print the list after removing the loop
     cout << "List after loop removal: ";
     printList(head);
 
-    // Reverse the list
+
     head = reverseList(head);
     cout << "Reversed List: ";
     printList(head);
